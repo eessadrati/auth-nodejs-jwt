e07ab3ac44057f958ddbe32105c44f25eb9dae0b
keynavas <keynavas.36@gmail.com>
2022-06-16 14:41:04 +0100

    create index.js and  user model

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//index
+const dotenv = require('dotenv');
+const database = require('./config/database');
+const express = require("express");
+
+const app = express();
+
+dotenv.config();
+database.connect();
+app.use(express.json());
+
+// Logic goes here
+
+const port = process.env.PORT || 5010;
+
+// server listening 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
