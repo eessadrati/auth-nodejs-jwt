81882cbdb78d95ef3f603cd268f254a137898bf1
keynavas <keynavas.36@gmail.com>
2022-06-17 13:13:20 +0100

    create routes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-const { MONGO_URI } = process.env;
-
 exports.connect = () => {
   // Connecting to the database
+  
   mongoose
-    .connect(MONGO_URI)
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Successfully connected to MongoDB.");
     })
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const dotenv = require('dotenv');
 const database = require('./config/database');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 const express = require("express");
-
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require("cookie-parser");
 const app = express();
 
 dotenv.config();
 database.connect();
+// Middlewares
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  }));
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use(cookieParser());
 
 // Logic goes here
+
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
 
 const port = process.env.PORT || 5010;
 
 // server listening 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
